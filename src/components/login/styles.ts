e0dc336306ede79d9486312b835e81d92543e174
DongHyeonWon <dhe7700@naver.com>
2023-11-23 04:32:37 +0900

    🍀[FEAT] 로그인 ui 추가

---
+++ src/components/login/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const LoginSection = styled.section`
+  & {
+    ${tw`h-full flex flex-col justify-center items-center`}
+  }
+`;
+
+export const LoginTitleH1 = styled.h1`
+  & {
+    ${tw` text-xl font-Pretendard-ExtraBold`}
+  }
+`;
