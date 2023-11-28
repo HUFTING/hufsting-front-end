a8f9083fa9a2b9ccc65b2919f4d5b75de7281a4c
DongHyeonWon <dhe7700@naver.com>
2023-11-29 00:02:23 +0900

    feat: dropdown useReducer 추가

--- src/types/common/profile.ts
+++ src/types/common/profile.ts
@@ -1,7 +1,9 @@
-// import { type FormInitialStateType } from '../../components/signup/index';
-
-import { type actionType } from '@/components/signup';
 import { type Dispatch } from 'react';
+
+export interface DropDownActionType {
+  name: string;
+  value: string;
+}
 
 export interface DropDownTypes {
   dropDownState: string;
@@ -10,7 +12,7 @@ export interface DropDownTypes {
   dropDownName: string;
   isBorder?: boolean;
   required?: boolean;
-  onChange: Dispatch<actionType>;
+  onChange: Dispatch<DropDownActionType>;
   disabled: boolean;
   handleCloseDropDown: () => void;
 }
@@ -41,8 +43,6 @@ export type DropDownListType = Pick<
   DropDownTypes,
   'dropDownItems' | 'dropDownName' | 'onChange' | 'handleCloseDropDown'
 >;
-
-// export type
 
 export type DropDownType = Pick<
   DropDownTypes,
