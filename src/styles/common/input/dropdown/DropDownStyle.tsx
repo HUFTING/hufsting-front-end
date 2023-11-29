6d68d4b8b875fd473e1c842a3eb35a9d9bbf8e05
DongHyeonWon <dhe7700@naver.com>
2023-11-29 12:47:41 +0900

    refac: dropdown 스타일 수정 및 items 순서 수정

--- src/styles/common/input/dropdown/DropDownStyle.tsx
+++ src/styles/common/input/dropdown/DropDownStyle.tsx
@@ -7,17 +7,14 @@ interface DropDownStyleType {
 
 export const DropDownStyle = styled.div<DropDownStyleType>`
   & {
-    ${tw`flex relative w-fit`}
+    ${tw`flex relative w-24`}
     >button {
       border: ${props => (props.$isBorder ? '1px solid #C8C8C8' : 'none')};
       color: ${props => (props.$isBorder ? '#5C7899' : 'black')};
-      ${tw`pl-2 flex rounded-2xl gap-2 items-center font-Pretendard-Regular w-full `}
-      > svg {
-        ${tw`mr-2`}
-      }
+      ${tw`pl-2 flex justify-end rounded-2xl gap-2 items-center font-Pretendard-Regular w-full `}
     }
     > ul {
-      ${tw`absolute top-7 z-10 w-full bg-white border-2 rounded-lg max-h-40 overflow-y-auto`}
+      ${tw`absolute top-7 z-10 w-20 bg-white border-2 rounded-lg max-h-40 overflow-y-auto`}
       > li {
         ${tw`px-2 py-2 border-b-2  cursor-pointer hover:bg-[#FF6969] hover:text-white`}
       }
