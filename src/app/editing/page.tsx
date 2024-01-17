ee0ff8628fb91dfb24b2693533511fa890e5f1d5
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2024-01-17 14:29:01 +0900

    [feat]: Button 디자인 수정, 오류 해결
    인원추가삭제버튼 -> SmallButtonPlus, SmallButtonMinus 컴포넌트 두개를 합쳐서 만듦 / 인원 수 처음에 0이 아닌 1로 설정되어있도록 설정 / 자잘한 css 조정

--- src/app/editing/page.tsx
+++ src/app/editing/page.tsx
@@ -9,117 +9,10 @@ import NameList from '@/components/NameList';
 import BackIcon from '@/components/common/ui/BackIcon';
 import BasicButton from '@/components/common/button/Button';
 import Modal from '@/components/Modal';
-import SmallButton from '@/components/common/button/SmallButton';
+import SmallButtonPlus from '@/components/common/button/SmallButtonPlus';
+import SmallButtonMinus from '@/components/common/button/SmallButtonMinus';
 
 const otherInfo = 'https://open.kakao.com/o/gto74LSf';
-
-const total = {
-  huftingid: 1,
-  gender: '남',
-  num: 2,
-};
-
-const Editing = () => {
-  const [isOpenModal, setOpenModal] = useState<boolean>(false);
-  /* const [gender, setGender] = useState<string | null>(null); */
-  const [numberOfPeople, setNumberOfPeople] = useState<number>(0);
-
-  /* const handleGenderSelection = (selectedGender: string) => {
-    setGender(selectedGender);
-  }; */
-
-  const handleIncrement = () => {
-    setNumberOfPeople(prev => prev + 1);
-  };
-
-  const handleDecrement = () => {
-    setNumberOfPeople(prev => prev - 1);
-  };
-
-  const handleClick = () => {
-    alert('clicked!');
-  };
-  const handleMore = useCallback(() => {
-    setOpenModal(!isOpenModal);
-  }, [isOpenModal]);
-
-  return (
-    <Container>
-      <Header>
-        <LogoIcon width={118} height={30} />
-        <div>
-          <SearchIcon />
-          <HamburgerIcon />
-        </div>
-      </Header>
-      <div className="titlebox">
-        <BackIcon />
-        <Title>훕팅 수정</Title>
-      </div>
-      {isOpenModal && (
-        <Modal handleMore={handleMore}>이곳에 children이 들어갑니다.</Modal>
-      )}
-      <div className="otherInfo">
-        <SubTitle>희망 인원 수</SubTitle>
-        <div className="flex">
-          <span>{numberOfPeople}</span>
-          <SmallButton
-            content="+"
-            onClickEvent={() => {
-              handleIncrement();
-            }}
-            isActive
-          />
-
-          <SmallButton
-            content="-"
-            onClickEvent={() => {
-              handleDecrement();
-            }}
-            isActive={false}
-          />
-        </div>
-
-        <OtherInfo>
-          <div className="top">
-            <SubTitle>오픈채팅방 링크</SubTitle>
-            <a href={otherInfo} target="_blank" rel="noopener noreferrer">
-              {otherInfo}
-            </a>
-            <More onClick={handleMore}>더보기</More>
-          </div>
-        </OtherInfo>
-      </div>
-      <div className="listbox">
-        <NameList total={total} />
-      </div>
-      <BasicButtonWrapper
-        style={{ display: 'flex', justifyContent: 'space-evenly' }}
-      >
-        <BasicButton
-          color="red"
-          assetType="Primary"
-          size="S"
-          content="삭제하기"
-          onClickEvent={handleClick}
-          isActive
-          buttonType="button"
-          width="40"
-        />
-        <BasicButton
-          color="red"
-          assetType="Primary"
-          size="S"
-          content="수정하기"
-          onClickEvent={handleClick}
-          isActive
-          buttonType="button"
-          width="40%"
-        />
-      </BasicButtonWrapper>
-    </Container>
-  );
-};
 
 const Container = styled.div`
   padding: 33px 0 0 0;
@@ -159,6 +52,24 @@ const Container = styled.div`
       margin-bottom: 30px;
     }
   }
+
+  .flex {
+    display: flex;
+    gap: 10px; /* 요소들 사이의 간격 설정 */
+    .numberContainer {
+      width: 74px;
+      height: 30px;
+      flex-shrink: 0;
+      border-radius: 10px;
+      background: #f3f5f7;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .SmallButtonContainer {
+      width: 300px;
+    }
+  }
 `;
 
 const Header = styled.div`
@@ -193,19 +104,24 @@ const SubTitle = styled.p`
 
 const OtherInfo = styled.div`
   .top {
+    margin-top: 20px; // 링크와 버튼 사이의 간격 조절
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-start; // align-items를 flex-start로 변경
   }
+  .top > * {
+    margin-bottom: 2px; // SubTitle과 링크 사이의 간격을 조절
+  }
+
   .infobox p {
     font-size: 18px;
   }
 `;
 
-const More = styled.button`
+/* const More = styled.button`
   font-size: 15px;
   color: #8d8d8d;
-`;
+`; */
 
 const BasicButtonWrapper = styled.div`
   display: flex;
@@ -218,5 +134,145 @@ const BasicButtonWrapper = styled.div`
   padding: 22px;
   z-index: 1000;
 `;
+
+const total = {
+  huftingid: 1,
+  gender: '남',
+  num: 1,
+};
+
+const Editing = () => {
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  /* const [gender, setGender] = useState<string | null>(null); */
+  const [numberOfPeople, setNumberOfPeople] = useState<number>(1);
+
+  /* const handleGenderSelection = (selectedGender: string) => {
+    setGender(selectedGender);
+  }; */
+
+  const handleIncrement = () => {
+    setNumberOfPeople(prev => {
+      const incrementedValue = prev + 1;
+      // total.num을 numberOfPeople과 동일하게 업데이트
+      total.num = incrementedValue;
+      return incrementedValue;
+    });
+  };
+
+  const handleDecrement = () => {
+    if (numberOfPeople > 1) {
+      setNumberOfPeople(prev => {
+        const decrementedValue = prev - 1;
+        // total.num을 numberOfPeople과 동일하게 업데이트
+        total.num = decrementedValue;
+        return decrementedValue;
+      });
+    }
+  };
+
+  const handleClick = () => {
+    alert('clicked!');
+  };
+  const handleMore = useCallback(() => {
+    setOpenModal(!isOpenModal);
+  }, [isOpenModal]);
+
+  const handleCopyLink = () => {
+    navigator.clipboard
+      .writeText(otherInfo)
+      .then(() => {
+        alert('링크가 클립보드에 복사되었습니다.');
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('링크 복사에 실패했습니다.', error);
+      });
+  };
+
+  return (
+    <Container>
+      <Header>
+        <LogoIcon width={118} height={30} />
+        <div>
+          <SearchIcon />
+          <HamburgerIcon />
+        </div>
+      </Header>
+      <div className="titlebox">
+        <BackIcon />
+        <Title>훕팅 수정</Title>
+      </div>
+      {isOpenModal && (
+        <Modal handleMore={handleMore}>이곳에 children이 들어갑니다.</Modal>
+      )}
+      <div className="otherInfo">
+        <SubTitle>희망 인원 수</SubTitle>
+        <div className="flex">
+          <div className="numberContainer">
+            <span>{numberOfPeople}</span>
+          </div>
+          <div className="SmallButtonContainer">
+            <SmallButtonMinus
+              content="-"
+              onClickEvent={() => {
+                handleDecrement();
+              }}
+              isActive={false}
+            />
+            <SmallButtonPlus
+              content="+"
+              onClickEvent={() => {
+                handleIncrement();
+              }}
+              isActive
+            />
+          </div>
+        </div>
+
+        <OtherInfo>
+          <div className="top">
+            <SubTitle>오픈채팅방 링크</SubTitle>
+            <a href={otherInfo} target="_blank" rel="noopener noreferrer">
+              {otherInfo}
+            </a>
+
+            {/* <More onClick={handleMore}>더보기</More> */}
+          </div>
+          {/* 복사 버튼 추가 */}
+          <button type="button" onClick={handleCopyLink}>
+            주소 복사
+          </button>
+        </OtherInfo>
+      </div>
+      <div className="listbox">
+        <NameList total={total} />
+      </div>
+      <BasicButtonWrapper
+        style={{ display: 'flex', justifyContent: 'space-evenly' }}
+      >
+        <BasicButton
+          color="red"
+          assetType="Primary"
+          size="S"
+          content="삭제하기"
+          onClickEvent={handleClick}
+          isActive
+          buttonType="button"
+          width="40"
+        />
+        <BasicButton
+          color="red"
+          assetType="Primary"
+          size="S"
+          content="수정완료"
+          onClickEvent={handleClick}
+          isActive
+          buttonType="button"
+          width="40%"
+        />
+      </BasicButtonWrapper>
+    </Container>
+  );
+};
 
 export default Editing;
