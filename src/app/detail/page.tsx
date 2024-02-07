45fc6cf6094262c42a1515d66478490231e2b5ee
kye1115z <kye1115z@naver.com>
2024-02-07 17:43:34 +0900

    feat: api 연동 로직 & NameList 수정 & 연동에 따른 객체 수정

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-// import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import NameList from '@/components/list/NameList';
 import BasicButton from '@/components/common/button/Button';
@@ -9,55 +10,31 @@ import SubHeader from '@/components/common/layout/SubHeader';
 import MainHeader from '@/components/common/layout/MainHeader';
 import MainInfo from '@/components/common/modal/MainInfo';
 
-const otherInfo = [
-  {
-    id: 1,
-    username: '김**',
-    major: 'Global Business%Technology 학부',
-  },
-  {
-    id: 2,
-    username: '원**',
-    major: '수학과',
-  },
-];
+interface Participant {
+  id: number;
+  name: string;
+  major: string;
+  studentNumber: string;
+  age: string;
+  mbti: string;
+  content: string;
+}
 
-const total = {
-  huftingid: 1,
-  gender: '남',
-  num: 2,
-};
-
-const userInfo = [
-  {
-    id: 1,
-    gender: '여',
-    username: '김**',
-    major: 'GBT학부',
-    stID: 202100000,
-    age: 2002,
-    mbti: 'ESFJ',
-    introduce: '즐거운 훕팅 많이 많이 이용해주세요~',
-    public: true,
-  },
-  {
-    id: 2,
-    gender: '남',
-    username: '원**',
-    major: 'GBT학부',
-    stID: 202100000,
-    age: 2002,
-    mbti: 'ESFJ',
-    introduce: '즐거운 훕팅 많이 많이 이용해주세요~',
-    public: true,
-  },
-];
+interface ListType {
+  id: number;
+  content: string;
+  matchingStatus: boolean;
+  title: string;
+  desiredNumPeople: number;
+  gender: string;
+  authorName: string;
+  participants: Participant[];
+}
 
 const Detail = () => {
-  // const router = useRouter();
-  // const { query } = router;
-  // const matchingPostId = query.matchingPostId;
-  // console.log(matchingPostId);
+  // 쿼리 받아오기
+  const searchParams = useSearchParams();
+  const search = searchParams.get('id');
 
   // 더 보기
   const [isOpenModal, setOpenModal] = useState(false);
@@ -67,79 +44,103 @@ const Detail = () => {
   };
 
   // 리스트 받아오기
-  // useEffect(() => {
-  //   // get
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `http://www.hufsting.com:8080/api/v1/matchingposts/${matchingPostId}`,
-  //         {
-  //           method: 'GET',
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //           },
-  //         },
-  //       );
-  //       if (!response.ok) {
-  //         throw new Error('error');
-  //       }
+  const [postInfo, setPostInfo] = useState<ListType | null>(null);
 
-  //       const data = await response.json();
+  useEffect(() => {
+    axios
+      .get(`http://www.hufsting.com:8080/api/v1/matchingposts/${search}`)
+      .then(res => {
+        const { data } = res;
+        setPostInfo(data);
+      })
+      .catch(error => {
+        // 사용자에게 오류 메시지를 표시하는 대신 다른 작업을 수행할 수 있습니다.
+        alert(
+          `데이터를 불러오는 중 오류가 발생했습니다. 나중에 다시 시도해주세요.${error}`,
+        );
+      });
+  }, [search]);
 
-  //     } catch (error) {
-  //       console.error('데이터를 가져오는 동안 오류 발생:', error);
-  //     }
-  //   };
+  // 훕팅 신청 api
+  const onApplyClick = () => {
+    const RequestData = {
+      matchingPostId: search,
+      participantIds: [1], // 수정 필요
+    };
 
-  //   fetchData();
-  // }, []);
+    axios
+      .post('http://www.hufsting.com:8080/api/v1/matchingrequests', RequestData)
+      .then(res => {
+        // console.log(res.data);
+        window.location.href = 'http://localhost:3000/result';
+      })
+      .catch(error => {
+        // 사용자에게 오류 메시지를 표시하는 대신 다른 작업을 수행할 수 있습니다.
+        alert(
+          `데이터를 불러오는 중 오류가 발생했습니다. 나중에 다시 시도해주세요.${error}`,
+        );
+      });
+  };
 
   return (
     <Container>
       <MainHeader />
       <SubHeader title="훕팅 참여하기" />
-      {isOpenModal && (
-        <MainInfo
-          handleMore={handleMore}
-          isModal={isOpenModal}
-          userInfo={userInfo}
-        />
-      )}
-      <div className="otherInfo">
-        <SubTitle>성별</SubTitle>
-        <div className="genderbox">
-          <p>{total.gender}성</p>
-        </div>
-        <OtherInfo>
-          <div className="top">
-            <SubTitle>상대 정보</SubTitle>
-            <More onClick={handleMore}>더 보기</More>
-          </div>
-          {otherInfo.map(info => (
-            <div className="infobox" key={info.id}>
-              <p>
-                {info.username} _ {info.major}
-              </p>
+      {postInfo !== null && (
+        <>
+          {isOpenModal && (
+            <MainInfo
+              desiredNumPeople={postInfo.desiredNumPeople}
+              handleMore={handleMore}
+              isModal={isOpenModal}
+              userInfo={postInfo.participants}
+            />
+          )}
+
+          <Title>{postInfo.title}</Title>
+          <div className="otherInfo">
+            <SubTitle>성별</SubTitle>
+            <div className="genderbox">
+              <p>{postInfo.gender}성</p>
             </div>
-          ))}
-        </OtherInfo>
-      </div>
-      <div className="listbox">
-        <p className="essential">필수 참가 인원 {total.num}인</p>
-        <NameList total={total} />
-      </div>
-      <BasicButtonWrapper>
-        <BasicButton
-          color="red"
-          assetType="Primary"
-          size="M"
-          content="매칭하기"
-          onClickEvent={null}
-          isActive
-          buttonType="button"
-          width="100%"
-        />
-      </BasicButtonWrapper>
+            <OtherInfo>
+              <div className="top">
+                <SubTitle>상대 정보</SubTitle>
+                <More onClick={handleMore}>더 보기</More>
+              </div>
+              {postInfo.participants.map(info => (
+                <div className="infobox" key={info.id}>
+                  <p>
+                    {info.name} _ {info.major}
+                  </p>
+                </div>
+              ))}
+            </OtherInfo>
+          </div>
+          <div className="listbox">
+            <p className="essential">
+              필수 참가 인원 {postInfo.desiredNumPeople}인
+            </p>
+            <NameList
+              desiredNumPeople={postInfo.desiredNumPeople}
+              participants={postInfo.participants}
+              editable
+            />
+          </div>
+          <BasicButtonWrapper>
+            <BasicButton
+              color="red"
+              assetType="Primary"
+              size="M"
+              content="훕팅 신청"
+              onClickEvent={onApplyClick}
+              isActive
+              buttonType="button"
+              width="100%"
+            />
+          </BasicButtonWrapper>
+        </>
+      )}
     </Container>
   );
 };
@@ -151,6 +152,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin-bottom: 50px;
 
   .titlebox {
     padding: 2px 22px;
@@ -160,7 +162,7 @@ const Container = styled.div`
   }
 
   .otherInfo {
-    padding: 25px 22px;
+    padding: 10px 22px 20px 22px;
 
     .genderbox {
       margin-bottom: 20px;
@@ -189,6 +191,12 @@ const Container = styled.div`
       margin-bottom: 30px;
     }
   }
+`;
+
+const Title = styled.p`
+  padding: 10px 22px;
+  font-size: 20px;
+  font-weight: bold;
 `;
 
 const SubTitle = styled.p`
