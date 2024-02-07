f1bbb37819e0cf0b1632bc7d59c10c6dad8b4c16
kye1115z <kye1115z@naver.com>
2024-02-08 04:49:31 +0900

    feat: 메이트 불러오기 컴포넌트 생성& 적용

---
+++ src/components/common/modal/ImportMateModal.tsx
@@ -0,0 +1,162 @@
+'use client';
+
+import React, { useCallback, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { FollowerListItemStyle } from '@/styles/followerStyles';
+import styled from 'styled-components';
+
+export interface UserInfo {
+  id: number;
+  name: string;
+  email: string;
+  photo: string;
+  profile: string;
+  isFollowing: boolean;
+}
+
+interface MainInfoProps {
+  isModal?: boolean;
+  handleMore?: () => void;
+  onUserSelect?: (userId: number) => void;
+}
+
+const ImportMateModal: React.FC<MainInfoProps> = ({
+  isModal,
+  handleMore,
+  onUserSelect,
+}) => {
+  const [userList, setUserList] = useState<UserInfo[] | null>(null);
+
+  const photolink =
+    'https://post-phinf.pstatic.net/MjAxOTA0MjVfMjYz/MDAxNTU2MTgyMDcxMjA3.5swDAjuEttvddnRBOvv8_7DG1KpVLdDQ7GaA-7lEnMYg.xB3rxsq0u1vVb3Jh5DJTHWCdYuFnc535Zvw5UXtkizcg.JPEG/IXmR_mz8JgIj4ScGz0Ve1yCbTQkY.jpg?type=w400';
+
+  const getUserList = useCallback(() => {
+    setUserList([
+      {
+        id: 1,
+        name: '김예은',
+        email: 'email',
+        photo: photolink,
+        profile: 'hi',
+        isFollowing: true,
+      },
+      {
+        id: 2,
+        name: '김강민',
+        email: 'email',
+        photo: photolink,
+        profile: 'hi',
+        isFollowing: false,
+      },
+    ]);
+  }, []);
+
+  useEffect(() => {
+    getUserList();
+  }, [isModal, handleMore, onUserSelect]);
+
+  // 사용자 선택
+  const handleUserSelect = (userId: number) => {
+    onUserSelect?.(userId);
+  };
+
+  return (
+    <Container>
+      {isModal != null && isModal && (
+        <Backdrop
+          onClick={(e: React.MouseEvent) => {
+            e.preventDefault();
+            handleMore != null && handleMore();
+          }}
+        />
+      )}
+      {isModal != null && isModal && (
+        <ModalContainer>
+          <Bar>&nbsp;</Bar>
+          <Header>
+            <span>메이트</span>를 선택해주세요.
+          </Header>
+          {userList?.map((user, index) => (
+            <FollowerListItemStyle
+              key={user.id}
+              role="presentation"
+              onClick={() => {
+                handleUserSelect(user.id);
+                if (handleMore !== null && handleMore !== undefined) {
+                  handleMore();
+                }
+              }}
+            >
+              <div className="photo">
+                <Image src={user.photo} layout="fill" alt="mate-photo" />
+              </div>
+              <div className="user">
+                <div>
+                  <div>{user.name}</div>
+                  <div>{user.profile}</div>
+                </div>
+              </div>
+            </FollowerListItemStyle>
+          ))}
+        </ModalContainer>
+      )}
+    </Container>
+  );
+};
+
+export default ImportMateModal;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+const ModalContainer = styled.div`
+  width: 370px;
+  min-height: 200px;
+  height: 83vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  bottom: 0px;
+  z-index: 9999;
+  background-color: #f3f5f7;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  overflow-y: auto;
+`;
+
+const Backdrop = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+`;
+
+const Bar = styled.div`
+  min-width: 46px;
+  height: 6px;
+  background-color: #c8c8c8;
+  border-radius: 50px;
+  margin-top: 8px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px;
+  font-size: 20px;
+  font-weight: bold;
+
+  span {
+    color: #ff324b;
+  }
+`;
