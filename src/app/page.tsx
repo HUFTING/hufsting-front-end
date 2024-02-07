f6bc634aa6bb46a9968bab3d42cc021a6f712dbe
kye1115z <kye1115z@naver.com>
2024-02-08 02:12:57 +0900

    feat: 기본 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import axiosInstance from '@/api/axiosInstance';
-import BackIcon from '@/components/common/ui/BackIcon';
-import LogoIcon from '@/components/common/ui/LogoIcon';
 import React from 'react';
+import Home from './home/page';
 
 export default async function Page() {
   await axiosInstance.get('/api/hello');
-  return (
-    <div>
-      <LogoIcon />
-      <BackIcon />
-    </div>
-  );
+  return <Home />;
 }
