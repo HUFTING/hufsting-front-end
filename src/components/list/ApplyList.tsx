4464ce43c66a7c122c58efa7e1877f43953adc83
kye1115z <kye1115z@naver.com>
2024-02-09 05:20:30 +0900

    feat: ApplyList 생성

---
+++ src/components/list/ApplyList.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+interface ApplyListProps {
+  lists: Array<{
+    matchingRequestId: number;
+    matchingRequestTitle: string;
+  }>;
+  pathnameProp?: string;
+}
+
+const ApplyList = ({ lists, pathnameProp }: ApplyListProps) =>
+  lists?.length > 0 && (
+    <Container>
+      {lists.map((item, index) => (
+        <Link
+          key={item.matchingRequestId}
+          href={{
+            pathname: pathnameProp,
+            query: { id: item.matchingRequestId },
+          }}
+          style={{ width: '100%' }}
+        >
+          <p>{item.matchingRequestTitle}</p>
+        </Link>
+      ))}
+    </Container>
+  );
+
+export default ApplyList;
+
+const Container = styled.div`
+  p {
+    font-size: 15px;
+  }
+`;
