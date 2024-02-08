494811767def32d2a0acf7939fb46f25161eb72a
DongHyeonWon <dhe7700@naver.com>
2024-02-08 13:08:33 +0900

    fix: baseURL 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -18,7 +18,7 @@ const setInterceptors = (instance: AxiosInstance) => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
