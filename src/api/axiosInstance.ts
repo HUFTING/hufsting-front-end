46270546713893914647f2d58d86400f07cc709a
DongHyeonWon <dhe7700@naver.com>
2024-02-08 12:21:01 +0900

    delete: 커밋 내용 삭제

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,26 @@
+import axios, { type AxiosInstance } from 'axios';
+
+const setInterceptors = (instance: AxiosInstance) => {
+  instance.interceptors.response.use(
+    response => response,
+    async error => {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        // 로그인이 필요합니다.
+        const loginError = new Error('로그인이 필요합니다.');
+        // eslint-disable-next-line @typescript-eslint/return-await
+        return Promise.reject(loginError);
+      }
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return instance;
+    },
+  );
+  return instance;
+};
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+});
+
+setInterceptors(axiosInstance);
+export default axiosInstance;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import axiosInstance from '@/api/axiosInstance';
 import React from 'react';
 import Home from './home/page';
 
-export default async function Page() {
-  await axiosInstance.get('/api/hello');
+export default function Page() {
   return <Home />;
 }
