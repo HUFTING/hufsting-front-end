41f990ecce8e79d9648432390240c0976824d858
kye1115z <kye1115z@naver.com>
2024-02-10 06:00:34 +0900

    feat: 대표자 로직 처리

--- src/components/list/ApplyList.tsx
+++ src/components/list/ApplyList.tsx
@@ -1,3 +1,4 @@
+import useUserDataStore from '@/store/user';
 import Link from 'next/link';
 import styled from 'styled-components';
 
@@ -7,31 +8,49 @@ interface ApplyListProps {
     matchingRequestTitle: string;
   }>;
   pathnameProp?: string;
+  representativeEmail: string;
 }
-
-const ApplyList = ({ lists, pathnameProp }: ApplyListProps) =>
-  lists?.length > 0 && (
-    <Container>
-      {lists.map((item, index) => (
-        <Link
-          key={item.matchingRequestId}
-          href={{
-            pathname: pathnameProp,
-            query: { id: item.matchingRequestId },
-          }}
-        >
-          <p>{item.matchingRequestTitle}</p>
-        </Link>
-      ))}
-    </Container>
+const ApplyList = ({
+  lists,
+  pathnameProp,
+  representativeEmail,
+}: ApplyListProps) => {
+  const userData = useUserDataStore(state => state.userData);
+  return (
+    lists?.length > 0 && (
+      <Container>
+        {lists.map((item, index) => (
+          <div key={item.matchingRequestId}>
+            {representativeEmail === userData.email ? (
+              <Link
+                href={{
+                  pathname: pathnameProp,
+                  query: { id: item.matchingRequestId },
+                }}
+              >
+                <p className="link">{item.matchingRequestTitle}</p>
+              </Link>
+            ) : (
+              <p className="none">{item.matchingRequestTitle}</p>
+            )}
+          </div>
+        ))}
+      </Container>
+    )
   );
+};
 
 export default ApplyList;
 
 const Container = styled.div`
-  p {
+  .link {
     font-size: 15px;
     text-decoration: underline;
-    text-underline-offset: 5px;
+    text-underline-offset: 3px;
+  }
+
+  .none {
+    font-size: 15px;
+    color: gray;
   }
 `;
